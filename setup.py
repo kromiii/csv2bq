07c4e37d907399ac00bfdf85d59b3589c38563df
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2023-09-15 21:25:40 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 setup(
     name="csv2bq",
+    description="upload local csv file to bigquery",
     version="0.1",
     author="Hiroyuki Kuromiya",
+    author_email="15026387+kromiii@users.noreply.github.com",
     packages=["csv2bq"],
 )
